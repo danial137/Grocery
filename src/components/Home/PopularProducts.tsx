e09b1ff11e8ea0904dfb4a4a0b9426ce8646c8ea
danial137 <danial79fakhrabadi@gmail.com>
2026-08-03 02:46:52 +0330

    feat: implement API call for popular products and remove dummy data; add authentication check in admin layout

--- src/components/Home/PopularProducts.tsx
+++ src/components/Home/PopularProducts.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react"
 import type { Product } from "../../types"
-import { dummyProducts } from "../../assets/assets"
 import { Link } from "react-router-dom"
 import { ArrowRightIcon } from "lucide-react"
 import ProductCard from "../ProductCard"
+import api from "../../config/api"
+import toast from "react-hot-toast"
 
 
 const PopularProducts = () => {
@@ -11,44 +12,48 @@ const PopularProducts = () => {
     const [products, setProducts] = useState<Product[]>([])
 
     useEffect(() => {
+        api.get('/products?limit=8&sort=rating').then(({ data }) => {
+            setProducts(data.products)
 
-        setProducts(dummyProducts.slice(0,10))
-        
+        }).catch((error: any) => {
+            toast.error(error.response.data.message || error?.message)
+        })
 
-    },[])
 
-  return (
-      <section className="pb-16">
-          
-          <div className="max-w-7xl mx-auto">
-              
-              <div className="flex items-center justify-between mb-8">
-                  
-                  <div>
-                      <h2 className="text-2xl font-semibold">Popular Products</h2>
-                      <p className="text-sm text-app-text-light mt-1">Top-rated products this season</p>
-                  </div>
+    }, [])
 
-                  <Link className="text-sm font-semibold text-app-orange hover:text-app-orange-dark flex items-center gap-1 transition-colors" to='/products'>
-                      
-                      View All <ArrowRightIcon className="size-4"/>
-                  
-                  </Link>
-                  
-              </div>
-              
-          </div>
+    return (
+        <section className="pb-16">
 
-          <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-4 xl:gap-8">
-              
-              {products.map((product) => (
-                <ProductCard product={product} key={product._id}/>
-            ))}
-              
-          </div>
-          
-    </section>
-  )
+            <div className="max-w-7xl mx-auto">
+
+                <div className="flex items-center justify-between mb-8">
+
+                    <div>
+                        <h2 className="text-2xl font-semibold">Popular Products</h2>
+                        <p className="text-sm text-app-text-light mt-1">Top-rated products this season</p>
+                    </div>
+
+                    <Link className="text-sm font-semibold text-app-orange hover:text-app-orange-dark flex items-center gap-1 transition-colors" to='/products'>
+
+                        View All <ArrowRightIcon className="size-4" />
+
+                    </Link>
+
+                </div>
+
+            </div>
+
+            <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-4 xl:gap-8">
+
+                {products.map((product) => (
+                    <ProductCard product={product} key={product._id} />
+                ))}
+
+            </div>
+
+        </section>
+    )
 }
 
 export default PopularProducts
